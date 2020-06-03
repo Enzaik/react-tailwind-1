548d47a56bc43cd5ab2aafbb5ae833616df3c500
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-06-03 18:41:51 +0200

    feat: change layout a bit

--- src/ListItemVertical.jsx
+++ src/ListItemVertical.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 function ListItem({ url }) {
   return (
-    <div class="bg-white border rounded-lg overflow-hidden max-w-md max-h-md mx-auto my-4 shadow-xl">
-      <img className="h-56 w-full object-cover" src={url} />
+    <div class="bg-white border rounded-lg overflow-hidden max-w-4xl max-h-xl mx-auto my-4 shadow-xl">
+      <div class="relative pb-2/3">
+        <img className="absolute h-full w-full object-cover" src={url} />
+      </div>
       <div class="p-6">
-        <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-100 text-teal-800">
-          New
-        </span>
-        <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide">beds &bull; baths</div>
-        <h4 class="font-semibold text-lg leading-tight truncate">Nommal</h4>
+        <div className="flex items-baseline">
+          <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">
+            New
+          </span>
+          <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">beds &bull; baths</div>
+        </div>
+        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">Casa en las afueras de la ciudad</h4>
         <div class="mt-1">
           $80
           <span class="text-gray-600 text-sm"> / wk</span>
