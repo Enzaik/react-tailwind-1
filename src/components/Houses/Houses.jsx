e4c45ea2536901fdc5ec671a7b84cd1a0cf0a71d
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-07-13 13:43:30 +0200

    feat: comment stuff

--- src/components/Houses/Houses.jsx
+++ src/components/Houses/Houses.jsx
@@ -10,7 +10,7 @@ function Houses() {
       <div className="block md:hidden">
         <main className="max-w-7xl pt-content mx-auto py-6 sm:px-6 md:ml-1/5 lg:px-8">
           <div className="flex px-2 justify-between items-baseline">
-            <div>Casas</div>
+            <div className="font-semibold">Casas</div>
             <div className="text-sm text-indigo-600">Ver más</div>
           </div>
           <CustomHits />
@@ -19,7 +19,7 @@ function Houses() {
       <div className="hidden md:block">
         <main className=" pt-24 mx-auto py-6 sm:px-6 margin-filter lg:px-8">
           <div className="flex px-2 justify-between items-baseline">
-            <div className="text-xl">Casas</div>
+            <div className="font-semibold">Casas</div>
             <div className="text-sm text-indigo-600">Ver más</div>
           </div>
           <CustomHits />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar(filterHandler = null, shouldShowBar = null) {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
+            {/* <button
               className="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-300 focus:outline-none focus:text-gray-300 focus:bg-gray-600 transition duration-150 ease-in-out"
               aria-label="Notifications"
             >
@@ -82,7 +82,7 @@ function Navbar(filterHandler = null, shouldShowBar = null) {
                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 />
               </svg>
-            </button>
+            </button> */}
 
             {/* <!-- Profile dropdown --> */}
             <Dropdown />
