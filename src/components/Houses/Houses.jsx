df59aa4953c8fdcfcf88fe72ae70efdeac52c03e
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-07-13 13:22:47 +0200

    fix: fix padding

--- src/components/Houses/Houses.jsx
+++ src/components/Houses/Houses.jsx
@@ -8,15 +8,16 @@ function Houses() {
     <>
       <VerticalFilter attribute="beds" searchable />
       <div className="block md:hidden">
-        <main className="max-w-7xl pt-24 mx-auto py-6 sm:px-6 md:ml-1/5 lg:px-8">
-          <div className="flex justify-between">
+        <main className="max-w-7xl pt-content mx-auto py-6 sm:px-6 md:ml-1/5 lg:px-8">
+          <div className="flex px-2 justify-between items-baseline">
             <div>Casas</div>
+            <div className="text-sm text-indigo-600">Ver más</div>
           </div>
           <CustomHits />
         </main>
       </div>
       <div className="hidden md:block">
-        <main className="pt-24 mx-auto py-6 sm:px-6 margin-filter lg:px-8">
+        <main className=" pt-24 mx-auto py-6 sm:px-6 margin-filter lg:px-8">
           <div className="flex px-2 justify-between items-baseline">
             <div className="text-xl">Casas</div>
             <div className="text-sm text-indigo-600">Ver más</div>
