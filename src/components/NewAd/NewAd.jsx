3261af22601d4c6025220e8b94d168844a960cef
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-08-01 16:09:09 +0200

    feat: form test 2

--- src/components/NewAd/NewAd.jsx
+++ src/components/NewAd/NewAd.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../Navbar/Navbar';
 
 function NewAd({ shouldShowBar }) {
-  return (
+  const truth = (
     <>
       <div className="fixed w-full z-30">
         <NavBar shouldShowBar={shouldShowBar} />
@@ -173,6 +173,38 @@ function NewAd({ shouldShowBar }) {
       </div>
     </>
   );
+
+  const wrong = (
+    <>
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+      <form method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </>
+  );
+  return wrong;
 }
 
 export default NewAd;
