98e60ce6201608aead0b2118b5279ec4c0b6b784
Ernesto Diaz <ernestoalejandro.diazcatala@grandparade.co.uk>
2020-08-03 16:11:44 +0200

    feat: misc

--- src/components/Filters/VerticalFilter.jsx
+++ src/components/Filters/VerticalFilter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../img/logo.svg';
 import CustomRefinementList from '../../CustomRefinedList';
 
 const Filters = ({ config }) => {
   return (
-    <div className="fixed h-screen hidden width-left-panel md:block pt-panel top-0 z-20 bg-gray-900">
-      <div className="w-full h-full bg-gray-800">
+    <div className="fixed h-screen hidden width-left-panel md:block top-0 z-20 bg-gray-900">
+      <div className="w-full h-full bg-gray-800 ">
+        <div className="flex justify-center py-3 h-16 border-b border-gray-900">
+          <Link to={'/'}>
+            <img className="block lg:hidden h-10 w-auto" src={logo} alt="Workflow logo" />
+          </Link>
+        </div>
         <CustomRefinementList config={config} searchable />
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -81,7 +81,7 @@ function Navbar({ filterHandler = null, shouldShowBar }) {
                     to={to}
                     className={`${orderClass} ${
                       to === activeLink ? 'border-b-2 border-indigo-500' : ''
-                    } inline-flex items-center px-1 pt-1  text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out`}
+                    } inline-flex items-center px-1 pt-1 text-white md:text-gray-900 text-sm font-medium leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out`}
                   >
                     {label}
                   </Link>
